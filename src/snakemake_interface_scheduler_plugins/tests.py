d530573f8d8f005f09f64b31dae4e85c82ac0f6c
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-10-18 15:04:45 +0200

    Code format

--- src/snakemake_interface_scheduler_plugins/tests.py
+++ src/snakemake_interface_scheduler_plugins/tests.py
@@ -144,18 +144,12 @@ class TestSchedulerBase(ABC):
             available_resources={"cpu": 1, "mem_mb": 2048},
             input_sizes=defaultdict(int),
         )
-        assert scheduled == [dag._jobs[0]], (
-            "Scheduler did not select the expected job"
-        )
+        assert scheduled == [dag._jobs[0]], "Scheduler did not select the expected job"
 
         dag._finished.add(dag._jobs[0])
 
         scheduled = scheduler.select_jobs(
-            [
-                dag._jobs[1],
-                dag._jobs[2],
-                dag._jobs[3]
-            ],
+            [dag._jobs[1], dag._jobs[2], dag._jobs[3]],
             dag._jobs,
             available_resources={"cpu": 5, "mem_mb": 10000},
             input_sizes=defaultdict(int),
